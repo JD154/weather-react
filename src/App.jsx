1bde2332202c20cf8599c5c57182194c002bf11f
JD154 <jesus.blancof96@gmail.com>
2020-08-08 15:36:56 -0400

    replaced default New York query for an empty state

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,18 @@ import convertDegrees from './utils/convertDegrees';
 import './App.css';
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('New York');
-  const [weather, setWeather] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
+  const [weather, setWeather] = useState({
+    clouds: '-',
+    icon: '00',
+    temp: 0,
+    feelsLike: 0,
+    minTemp: 0,
+    maxTemp: 0,
+    humidity: 0,
+    city: '-',
+    country: '-',
+  });
   const [isFahrenheit, setisFahrenheit] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -22,57 +32,49 @@ function App() {
   };
 
   useEffect(() => {
-    setIsLoading(true);
-    axios.get(`${api.URL}/weather?q=${searchQuery}&units=metric&APPID=${api.KEY}`)
-      .then((res) => {
-        const weatherData = {
-          clouds: res.data.weather[0].description,
-          icon: res.data.weather[0].icon,
-          temp: Math.round(res.data.main.temp),
-          feelsLike: Math.round(res.data.main.feels_like),
-          minTemp: Math.round(res.data.main.temp_min),
-          maxTemp: Math.round(res.data.main.temp_max),
-          humidity: res.data.main.humidity,
-          city: res.data.name,
-          country: res.data.sys.country,
-        };
-        setWeather(weatherData);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        const weatherData = {
-          clouds: '-',
-          icon: '01d',
-          temp: Math.round(0),
-          feelsLike: Math.round(0),
-          minTemp: Math.round(0),
-          maxTemp: Math.round(0),
-          humidity: 0,
-          city: '-',
-          country: '-',
-        };
-        setWeather(weatherData);
-        setIsLoading(false);
-        setError(true);
-        // eslint-disable-next-line no-console
-        console.log(err);
-      });
+    if (searchQuery !== '') {
+      setIsLoading(true);
+      axios.get(`${api.URL}/weather?q=${searchQuery}&units=metric&APPID=${api.KEY}`)
+        .then((res) => {
+          const weatherData = {
+            clouds: res.data.weather[0].description,
+            icon: res.data.weather[0].icon,
+            temp: Math.round(res.data.main.temp),
+            feelsLike: Math.round(res.data.main.feels_like),
+            minTemp: Math.round(res.data.main.temp_min),
+            maxTemp: Math.round(res.data.main.temp_max),
+            humidity: res.data.main.humidity,
+            city: res.data.name,
+            country: res.data.sys.country,
+          };
+          setWeather(weatherData);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(true);
+          // eslint-disable-next-line no-console
+          console.log(err);
+        });
+    }
     // eslint-disable-next-line
   }, [searchQuery]);
 
   useEffect(() => {
-    const weatherData = {
-      clouds: weather.clouds,
-      icon: weather.icon,
-      temp: convertDegrees(weather.temp, isFahrenheit),
-      feelsLike: convertDegrees(weather.feelsLike, isFahrenheit),
-      minTemp: convertDegrees(weather.minTemp, isFahrenheit),
-      maxTemp: convertDegrees(weather.maxTemp, isFahrenheit),
-      humidity: weather.humidity,
-      city: weather.city,
-      country: weather.country,
-    };
-    setWeather(weatherData);
+    if (searchQuery !== '') {
+      const weatherData = {
+        clouds: weather.clouds,
+        icon: weather.icon,
+        temp: convertDegrees(weather.temp, isFahrenheit),
+        feelsLike: convertDegrees(weather.feelsLike, isFahrenheit),
+        minTemp: convertDegrees(weather.minTemp, isFahrenheit),
+        maxTemp: convertDegrees(weather.maxTemp, isFahrenheit),
+        humidity: weather.humidity,
+        city: weather.city,
+        country: weather.country,
+      };
+      setWeather(weatherData);
+    }
     // eslint-disable-next-line
   }, [isFahrenheit]);
 
